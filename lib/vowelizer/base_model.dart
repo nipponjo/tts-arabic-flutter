64ab2b14a1b57b77c5e2ce29f990018059a8d7e9
nipponjo <28433296+nipponjo@users.noreply.github.com>
2025-03-29 18:55:25 +0100

    Improve imports

---
+++ lib/vowelizer/base_model.dart
@@ -0,0 +1,5 @@
+abstract class Vowelizer {
+  String vowelize(String text);
+  void initSessions({String modelPath});
+  void release();
+}
--- lib/vowelizer/model.dart
+++ lib/vowelizer/model.dart
@@ -1,7 +1,2 @@
+export 'base_model.dart';
 export 'shakkelha/model.dart';
-
-abstract class Vowelizer {
-  String vowelize(String text);
-  void initSessions({String modelPath});
-  void release();
-}
